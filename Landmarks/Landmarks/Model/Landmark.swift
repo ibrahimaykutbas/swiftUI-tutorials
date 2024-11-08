97fba9d03b77154a740a3782bf0c3eee0369647d
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2024-11-08 20:30:38 +0300

    feat: Composing complex interfaces

--- Landmarks/Landmarks/Model/Landmark.swift
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -3,21 +3,30 @@ import SwiftUI
 import CoreLocation
 
 struct Landmark: Hashable, Codable, Identifiable {
-    private var imageName: String
-    private var coordinates: Coordinates
-    
     var id: Int
     var name: String
     var park: String
     var state: String
     var description: String
+    var isFavorite: Bool
+    var isFeatured: Bool
+    
+    var category: Category
+    enum Category: String, CaseIterable, Codable {
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
+    
+    private var imageName: String
     var image: Image {
         Image(imageName)
     }
+    
+    private var coordinates: Coordinates
     var locationCoordinates: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
     }
-    var isFavorite: Bool
     
     struct Coordinates: Hashable, Codable {
         var latitude: Double
